6987c51c1edd11814bc9f8889a61b76455e08534
Kanika Rana <krana@redhat.com>
2020-08-13 08:44:45 -0400

    fixed import to fork

--- pkg/devfile/parser/data/versions.go
+++ pkg/devfile/parser/data/versions.go
@@ -4,8 +4,8 @@ import (
 	"reflect"
 
 	// v100 "github.com/devfile/parser/pkg/devfile/parser/data/1.0.0"
-	v200 "github.com/devfile/parser/pkg/devfile/parser/data/2.0.0"
-	v210 "github.com/devfile/parser/pkg/devfile/parser/data/2.1.0"
+	v200 "github.com/ranakan19/parser/pkg/devfile/parser/data/2.0.0"
+	v210 "github.com/ranakan19/parser/pkg/devfile/parser/data/2.1.0"
 )
 
 // SupportedApiVersions stores the supported devfile API versions
